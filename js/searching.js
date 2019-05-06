3ce99cc0cda7cf9982eea1b57b7574552757e8f0
zhe6289 <zhe1217_92@hotmail.com>
2019-05-06 23:51:07 +0800

    searching js

---
+++ js/searching.js
@@ -0,0 +1,31 @@
+function myFunction() {
+    var input, filter, ul, li, a, i, txtValue;
+    input = document.getElementById("myInput");
+    filter = input.value.toUpperCase();
+    ul = document.getElementById("myUL");
+    li = ul.getElementsByTagName("li");
+    for (i = 0; i < li.length; i++) {
+        a = li[i].getElementsByTagName("a")[0];
+        txtValue = a.textContent || a.innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+            li[i].style.display = "";
+        } else {
+            li[i].style.display = "none";
+        }
+    }
+}
+
+var $menu = $('#myInput');
+var $ul = $('#myUL');
+
+$(document).mouseup(function (e) {
+    if (!$menu.is(e.target) // if the target of the click isn't the container...
+        && $menu.has(e.target).length === 0) // ... nor a descendant of the container
+    {
+        $ul.css("display", "none");
+    }
+});
+
+$('input').on('click', () => {
+    $ul.css("display", "block");
+});
