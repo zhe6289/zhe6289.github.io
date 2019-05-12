7e6d85c81b590c7d78690160ac5d0fb862d4a04b
zhe6289 <zhe1217_92@hotmail.com>
2019-05-12 15:23:41 +0800

    nav

--- js/scroll.js
+++ js/scroll.js
@@ -2,13 +2,13 @@ $(document).ready(function () {
 
     $(window).on("scroll", function(e) {
         if($(window).scrollTop() >= 1 ) {
-              $('.navbar').removeClass('bg-dark').addClass('bg-light');
-              $('.navbar').removeClass('navbar-dark').addClass('navbar-light');
+              $('.navbar').removeClass('bg-light').addClass('bg-dark');
+              $('.navbar').removeClass('navbar-light').addClass('navbar-dark');
             }
             
             else {
-                $('.navbar').removeClass('bg-light').addClass('bg-dark');
-                $('.navbar').removeClass('navbar-light').addClass('navbar-dark');
+                $('.navbar').removeClass('bg-dark').addClass('bg-light');
+                $('.navbar').removeClass('navbar-dark').addClass('navbar-light');
         }
         
         var w = window.innerWidth;
